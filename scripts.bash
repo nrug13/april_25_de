11bf411abf1972c3801c6bf01d9a8ca0a6039a0b
nrug13 <nurgunganbarova@outlook.com>
2026-03-18 20:07:31 +0400

    metabase elave edildi

--- scripts.bash
+++ scripts.bash
@@ -3,5 +3,18 @@ cd /home/nurgun/Documents/homework_mar_14
 docker logs jupyter | grep -i token
 
 sudo chmod 777 -R ./notebooks
+http://127.0.0.1:8888/lab
 
 docker logs jupyter 2>&1 | grep "http://127.0.0.1:8888"
+
+
+sudo lsof -i :3000
+
+docker ps -a --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"
+
+docker exec -it jupyter bash wget 'url'
+docker logs metabase 2>&1 | tail -40
+
+
+
+docker volume rm feb14_pgdata_mydb
